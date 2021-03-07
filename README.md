3a4cd37aca7d1f684738956f04f6adc238b32f87
Reginaldo Motta <reginaldomcv@gmail.com>
2021-03-07 13:27:20 -0400

    Final
    project finish

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # questions-guide
 
-NodeJS training study project - Udemy
+# Backend study project with NodeJS
 
-Tools used: Express, EJS, Body-parse, Sequelize, MySQL ...
+### NodeJS training - Udemy
+
+Libraries and frameworks used:
+
+1. Express;
+2. EJS;
+3. Sequelize;
+4. MySQL;
+
+
+
+This project was carried out only targeting the backend training
--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
+/**
+ * Frameworks and Libraries
+ */
+const express = require('express');
 const connection = require("./database/database");
+const Question = require("./models/Question")
+const Answer = require("./models/Answer")
+const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+/**
+ * Connection database
+ */
 connection
     .authenticate()
     .then(() => {
@@ -12,25 +22,80 @@ connection
         console.log(msgErro);
     })
 
+/**
+ * EJS 
+ */
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
+/**
+ * Rotes
+ * */
 app.get("/", (req, res) => {
-    res.render("index");
+    Question.findAll({
+        raw: true, order: [
+            ['id', 'DESC']
+        ]
+    }).then(questions => {
+        res.render("index", {
+            questions: questions
+        });
+    });
 });
 
-app.get("/question", (req, res) => {
-    res.render("question");
-})
+app.get("/toask", (req, res) => {
+    res.render("toask");
+});
 
 app.post("/savequestion", (req, res) => {
     var title = req.body.title;
     var description = req.body.description;
-    res.send("Formulário recebido! título " + title + " " + " descrição " + description);
+    Question.create({
+        title: title,
+        description: description
+    }).then(() => {
+        res.redirect("/");
+    });
 });
 
+app.get("/question/:id", (req, res) => {
+    var id = req.params.id;
+    Question.findOne({
+        where: { id: id }
+    }).then(question => {
+        if (question != undefined) {
 
+            Answer.findAll({
+                where: { questionId: question.id },
+                order: [
+                    ['id', 'DESC']
+                ]
+            }).then(answers => {
+                res.render("question", {
+                    question: question,
+                    answers: answers
+                });
+            });
+
+        } else {
+            res.redirect("/");
+        }
+    });
+})
+
+app.post("/answer", (req, res) => {
+    var answerBody = req.body.answerBody;
+    var questionId = req.body.question;
+    Answer.create({
+        answerBody: answerBody,
+        questionId: questionId
+    }).then(() => {
+        res.redirect("/question/" + questionId);
+    });
+});
+
+/**
+ * Server
+ */
 app.listen(3333, () => { console.log("App running!"); });
